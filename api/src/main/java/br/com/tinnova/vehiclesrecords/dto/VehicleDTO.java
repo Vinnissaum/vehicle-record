6eea95e82834ac27f0daa94ab092f0817f85aade
Vinicius Braz <vinicius_lsb@live.com>
2023-01-04 19:28:38 -0300

    refactor: remove unused imports

--- api/src/main/java/br/com/tinnova/vehiclesrecords/dto/VehicleDTO.java
+++ api/src/main/java/br/com/tinnova/vehiclesrecords/dto/VehicleDTO.java
@@ -5,10 +5,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
