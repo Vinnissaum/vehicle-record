810476e7d5ac2f502be7d1379d646aba964eb2ae
Vinicius Braz <vinicius_lsb@live.com>
2023-01-04 11:58:41 -0300

    init api

---
+++ api/src/main/java/br/com/tinnova/vehiclesrecords/VehiclesRecordsApplication.java
@@ -0,0 +1,13 @@
+package br.com.tinnova.vehiclesrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesRecordsApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/com/tinnova/vehiclesrecords/VehiclesRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tinnova.vehiclesrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
