31f0f54890f164ab51bf24730be86a129b0f810f
Vinicius Braz <vinicius_lsb@live.com>
2023-01-04 13:05:09 -0300

    feat: implements vehicle entity and a brand enumeration

---
+++ api/src/main/java/br/com/tinnova/vehiclesrecords/vehicle/Brand.java
@@ -0,0 +1,13 @@
+package br.com.tinnova.vehiclesrecords.vehicle;
+
+public enum Brand {
+    VOLKSWAGEN,
+    FORD,
+    CHEVROLET,
+    AUDI,
+    HONDA,
+    FIAT,
+    BMW,
+    CITROEN,
+    TOYOTA
+}
---
+++ api/src/main/java/br/com/tinnova/vehiclesrecords/vehicle/Vehicle.java
@@ -0,0 +1,48 @@
+package br.com.tinnova.vehiclesrecords.vehicle;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id", updatable = false)
+    private UUID id;
+
+    @NotBlank(message = "Model is mandatory")
+    private String model;
+
+    @NotBlank(message = "Brand is mandatory")
+    @Enumerated(EnumType.STRING)
+    private Brand brand;
+
+    @NotNull(message = "Production year is mandatory")
+    @Column(name = "production_year")
+    private Integer year;
+
+    private String description;
+
+    private Boolean isSold;
+
+    @CreationTimestamp
+    @Column(nullable = false,  columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private LocalDateTime createAt;
+
+    @UpdateTimestamp
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private LocalDateTime updatedAt;
+
+}
