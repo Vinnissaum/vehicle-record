077730c27487318ea6c99c46c05603d751e03466
Vinicius Braz <vinicius_lsb@live.com>
2023-01-05 18:50:22 -0300

    fat: add link to back to home

--- web/src/components/Header/index.jsx
+++ web/src/components/Header/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import Container from './styles';
 
 export default function Header() {
   return (
     <Container>
-      <img src={logo} alt="Tinnova Software Solutions" style={{ width: 214 }} />
+      <Link to="/">
+        <img src={logo} alt="Tinnova Software Solutions" style={{ width: 214 }} />
+      </Link>
     </Container>
   );
 }
