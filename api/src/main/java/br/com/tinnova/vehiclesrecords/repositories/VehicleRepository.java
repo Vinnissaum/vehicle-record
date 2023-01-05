99949f6dbf7b6b5ccca4a23a670938952d3e314a
Vinicius Braz <vinicius_lsb@live.com>
2023-01-04 21:09:14 -0300

    feat: impl repository with spring query method

--- api/src/main/java/br/com/tinnova/vehiclesrecords/repositories/VehicleRepository.java
+++ api/src/main/java/br/com/tinnova/vehiclesrecords/repositories/VehicleRepository.java
@@ -4,7 +4,15 @@ import br.com.tinnova.vehiclesrecords.models.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle, UUID>, VehicleRepositoryCustom {}
+public interface VehicleRepository extends JpaRepository<Vehicle, UUID>, VehicleRepositoryCustom {
+
+    List<Vehicle> findByIsSoldFalse();
+
+    List <Vehicle> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+}
