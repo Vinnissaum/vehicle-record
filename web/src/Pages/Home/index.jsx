ae800bc65d782abc34bb968798c1abf4e6563f5f
Vinicius Braz <vinicius_lsb@live.com>
2023-01-05 18:52:55 -0300

    feat: implements home page

--- web/src/Pages/Home/index.jsx
+++ web/src/Pages/Home/index.jsx
@@ -1,18 +1,75 @@
-import React from 'react';
-import { Container, Header } from './styles';
-
-const vehicles = 'teste';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import VehicleService from '../../services/VehicleService';
+import { Container, Header, Card } from './styles';
+import Button from '../../components/Button';
 
 export default function Home() {
+  const [vehicles, setVehicles] = useState([]);
+
+  const loadVehicles = useCallback(async () => {
+    const vehicleList = await VehicleService.listVehicles();
+    setVehicles(vehicleList);
+  });
+
+  useEffect(() => {
+    loadVehicles();
+  }, [loadVehicles]);
+
+  async function handleDeleteVehicle(vehicleToDeleteId) {
+    await VehicleService.deleteVehicle(vehicleToDeleteId);
+    setVehicles((prevState) => prevState.filter(({ id }) => (
+      id !== vehicleToDeleteId
+    )));
+  }
+
   return (
     <Container>
       <Header>
         <strong>
-          Veículos:
+          {vehicles.length}
           {' '}
-          {vehicles}
+          {vehicles.length === 1 ? 'Veículo' : 'Veículos'}
         </strong>
+        <Link to="/cadastrar">Adicionar Veículo</Link>
       </Header>
+      {vehicles.map((vehicle) => (
+        <Card key={vehicle.id}>
+          <div className="info">
+            <div className="model">
+              <strong>{ vehicle.model }</strong>
+            </div>
+            <span>{vehicle.brand}</span>
+            <span>
+              Ano:
+              {' '}
+              {vehicle.year}
+            </span>
+            <span>
+              Vendido:
+              {' '}
+              {vehicle.isSold ? 'Sim' : 'Não'}
+            </span>
+          </div>
+
+          <div className="actions">
+            <Link to={`/editar/${vehicle.id}`} style={{ textDecoration: 'none' }}>
+              <Button
+                type="button"
+              >
+                Alterar
+              </Button>
+            </Link>
+
+            <Button
+              type="button"
+              onClick={() => handleDeleteVehicle(vehicle.id)}
+            >
+              Excluir
+            </Button>
+          </div>
+        </Card>
+      ))}
     </Container>
   );
 }
--- web/src/Pages/Home/styles.js
+++ web/src/Pages/Home/styles.js
@@ -13,6 +13,7 @@ export const Header = styled.header`
   margin-top: 32px;
   border-bottom: 2px solid ${({ theme }) => theme.colors.gray[200]};
   padding-bottom: 16px;
+  margin-bottom: 16px;
 
   strong {
     font-size: 24px;
@@ -31,6 +32,54 @@ export const Header = styled.header`
       color: #FFF;
       background: ${({ theme }) => theme.colors.primary.main};
       transform: scale(1.05);
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: ${({ theme }) => theme.colors.gray[700]};
+  color: ${({ theme }) => theme.colors.primary.main};
+  padding: 16px;
+  border-radius: 4px;
+  justify-content: space-between;
+  display: flex;
+  align-items: center;
+  transition: all 0.2s ease-in-out;
+
+  & + & {
+    margin-top:16px;
+  }
+
+  &:hover {
+    box-shadow: 0px 4px 10px rgba(0,0,0,0.1);
+    transform: translateY(-1px);
+  }
+
+  .info {
+    .model {
+      display: flex;
+      align-items: center;
+
+    }
+
+    span {
+      margin: 5px 0;
+      display: block;
+      font-size: 14px;
+      color: ${({ theme }) => theme.colors.gray[200]};
+    }
+
+  }
+
+  .actions {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+
+    button {
+      height: 32px;
+      width: auto;
     }
   }
 `;
