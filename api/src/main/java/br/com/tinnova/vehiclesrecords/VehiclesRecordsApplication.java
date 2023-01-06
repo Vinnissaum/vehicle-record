bfdc9e0dc59a46f011d86d35e032930bd4332eac
Vinicius Braz <vinicius_lsb@live.com>
2023-01-05 22:49:01 -0300

    chore: enable cors

--- api/src/main/java/br/com/tinnova/vehiclesrecords/VehiclesRecordsApplication.java
+++ api/src/main/java/br/com/tinnova/vehiclesrecords/VehiclesRecordsApplication.java
@@ -18,8 +18,8 @@ public class VehiclesRecordsApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000") //
-						.allowedMethods("GET", "POST","PUT", "DELETE");;
+				registry.addMapping("/**").allowedOrigins("*") //
+						.allowedMethods("GET", "POST","PUT", "DELETE");
 			}
 		};
 	}
